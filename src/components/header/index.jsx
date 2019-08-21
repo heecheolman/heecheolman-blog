ad17ac182097871c481057250dc0de312ba7df05
HeeCheolKim <msn9975@gmail.com>
2019-08-21 23:44:24 +0900

    test: darkmode

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,9 +3,22 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import HeadRoom from 'react-headroom';
 import './index.scss';
 
-export const Header = () => (
-  <StaticQuery
-    query={graphql`
+
+
+export const Header = () => {
+  let isDarkMode = false;
+
+  const toggleDarMode = () => {
+    // window.__THEME_MODE = 'light';
+    console.log('window.__THEME_MOMDE :: ', window.__THEME_MODE);
+    console.log('window.matchMedia :: ', window.matchMedia("(prefers-color-scheme: dark)"));
+    window.__THEME_MODE = window.__THEME_MODE === 'dark' ? 'light' : 'dark';
+    document.getElementsByTagName('html')[0].classList[window.__THEME_MODE === 'dark' ? 'add' : 'remove']('darkmode');
+  };
+
+  return (
+    <StaticQuery
+      query={graphql`
       query HeaderQuery {
         site {
           siteMetadata {
@@ -14,15 +27,19 @@ export const Header = () => (
         }
       }
     `}
-    render={data => (
-      <HeadRoom>
-        <header className="header">
-          <div className="header__title flex justify-between-sort">
-            <Link to="/" className="header__title__text hee-labeled">🎒{data.site.siteMetadata.title}</Link>
-          </div>
-        </header>
-      </HeadRoom>
-    )}
-  />
+      render={data => (
+        <HeadRoom>
+          <header className="header">
+            <div className="header__title flex justify-between-sort">
+              <Link to="/" className="header__title__text hee-labeled">🎒{data.site.siteMetadata.title}</Link>
+            </div>
+            <div onClick={() => toggleDarMode()}>
+              darkmode
+            </div>
+          </header>
+        </HeadRoom>
+      )}
+    />
 
-);
+  );
+}
