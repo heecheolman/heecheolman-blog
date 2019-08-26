2ec26120e458bb057284cfb2840ecf38bbe2db95
HeeCheolKim <msn9975@gmail.com>
2019-08-26 22:39:11 +0900

    fix: 다크모드 비활성화

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,19 +3,7 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import HeadRoom from 'react-headroom';
 import './index.scss';
 
-
-
 export const Header = () => {
-  let isDarkMode = false;
-
-  const toggleDarMode = () => {
-    // window.__THEME_MODE = 'light';
-    console.log('window.__THEME_MOMDE :: ', window.__THEME_MODE);
-    console.log('window.matchMedia :: ', window.matchMedia("(prefers-color-scheme: dark)"));
-    window.__THEME_MODE = window.__THEME_MODE === 'dark' ? 'light' : 'dark';
-    document.getElementsByTagName('html')[0].classList[window.__THEME_MODE === 'dark' ? 'add' : 'remove']('darkmode');
-  };
-
   return (
     <StaticQuery
       query={graphql`
@@ -32,9 +20,6 @@ export const Header = () => {
           <header className="header">
             <div className="header__title flex justify-between-sort">
               <Link to="/" className="header__title__text hee-labeled">🎒{data.site.siteMetadata.title}</Link>
-            </div>
-            <div onClick={() => toggleDarMode()}>
-              darkmode
             </div>
           </header>
         </HeadRoom>
